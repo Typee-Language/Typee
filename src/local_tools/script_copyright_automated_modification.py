f7d880b8437e6636def8b619a24826d868a3b813
Schmouk <ph.schmouker@yahoo.fr>
2019-01-16 17:32:32 +0100

    #128-Modify copyrights dates
    Done for every module in directory `src`.

--- src/local_tools/script_copyright_automated_modification.py
+++ src/local_tools/script_copyright_automated_modification.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 """
-Copyright (c) 2018 Philippe Schmouker, Typee project, http://www.typee.ovh
+Copyright (c) 2018-2019 Philippe Schmouker, Typee project, http://www.typee.ovh
 
 Permission is hereby granted,  free of charge,  to any person obtaining a copy
 of this software and associated documentation files (the "Software"),  to deal
